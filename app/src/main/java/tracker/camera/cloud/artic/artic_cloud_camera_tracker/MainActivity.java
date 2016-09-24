584a5482503fa4dd5047d6d6f9dc4f5308f7b2c8
Lrakulka <Olecsandr32@gmail.com>
2016-09-24 17:08:50 +0200

    Init commit

---
+++ app/src/main/java/tracker/camera/cloud/artic/artic_cloud_camera_tracker/MainActivity.java
@@ -0,0 +1,14 @@
+package tracker.camera.cloud.artic.artic_cloud_camera_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import cloud.artik.websocket.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
